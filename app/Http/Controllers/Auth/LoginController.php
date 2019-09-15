aec3a2728488a333327622f07f18fc70c3d285e6
rawdasherif <rawdasherif119@gmail.com>
2019-09-15 17:46:21 +0200

    use custom rule in google recaptcha instead of using sometimes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use App\Rules\GoogleRecaptcha;
 
 
 
@@ -53,7 +54,7 @@ class LoginController extends Controller
         $request->validate([
             $this->username() => 'required|string',
             'password' => 'required|string',
-            'g-recaptcha-response' => 'sometimes|recaptcha',
+            'g-recaptcha-response' => New GoogleRecaptcha() ,
         ]);
     }
 
