97c53d7df186fdf388b6adb7d1bac99f685d7929
rawdasherif <rawdasherif119@gmail.com>
2019-10-03 15:21:48 +0200

    fix role validation

--- app/Http/Requests/RoleRequest.php
+++ app/Http/Requests/RoleRequest.php
@@ -24,7 +24,7 @@ class RoleRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'=>'required|max:150|unique:roles,name'.$this->getId(),
+            'name'=>'required|max:150|unique:roles,name,'.$this->getId(),
             'description' => 'required|max:250',
         ];
     }
