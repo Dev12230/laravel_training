66a891d62e25269f81f33da3caa03af90e01f2d2
rawdasherif <rawdasherif119@gmail.com>
2019-10-23 17:41:04 +0200

    refactor doc

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -34,22 +34,26 @@ class ImagesController extends Controller
 
     /**
      * Retrieve stored images using ids of them.
+     * get class(News or Event) using (getImages(fn) URL)  using  ModelInstance trait
+     * find object from this class using $request
+     * get images of this object
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function getImages(Request $request)
     {
-        $model_object=app($this->getClass($request))::find($request->id);
+
+        $model_object=app($this->getClass($request))::find($request->id); 
         $images=$model_object->image;
 
         return response()->json($images);
     }
 
     /**
+     * get model Name(News or Event) using ( uploadToServers(fn) URL)  using  ModelInstance trait
      * Upload image direct to server
      * Create image in storage and store
-     *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
